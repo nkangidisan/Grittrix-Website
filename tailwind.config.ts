10c56872082732fb30522df216e623ac47714f68
Disan Nkangi <nkangidisan6@gmail.com>
2025-06-21 19:45:24 +0000

    now fix the issue of frequently  disappearing of stylings in my website

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
+
 import type { Config } from 'tailwindcss';
 
 const config = {
   darkMode: ['class'],
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   prefix: "",
   theme: {
