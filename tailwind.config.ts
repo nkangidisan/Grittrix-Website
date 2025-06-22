7f46d97ae510c2902db329191093fd1fd28f5db6
Disan Nkangi <nkangidisan6@gmail.com>
2025-06-22 20:33:10 +0000

    u review the project and u also see what u did for sure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,7 @@ import type { Config } from 'tailwindcss';
 const config = {
   darkMode: ['class'],
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   prefix: "",
   theme: {
