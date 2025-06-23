14338d2ce99f3c3eb0ea7a2cbb92e822c5133aa3
Disan Nkangi <nkangidisan6@gmail.com>
2025-06-23 23:56:34 +0000

    now push it upper and reduce its size a bit

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -70,7 +70,7 @@ export default function ServicesPage() {
                 fill 
                 className="object-cover" 
                 />
-                 <div className="hidden md:block absolute top-4 -left-4 w-40 h-40 animate-float opacity-80">
+                 <div className="hidden md:block absolute -top-24 -left-12 w-40 h-40 animate-float opacity-80">
                     <Image
                       src="/media/sphere-white.webp"
                       alt="Floating abstract white sphere"
